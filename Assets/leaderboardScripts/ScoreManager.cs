4564019486899fd8c1867a2f1526aac6d7a3141b
Tih1st <jijunsui@ymail.com>
2018-04-11 13:40:31 -0500

    j
    j

---
+++ Assets/leaderboardScripts/ScoreManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour
+{
+    // From the inspector, drag & Drop the GameObject holding the Text component used to display the score
+    [SerializeField]
+    private Text scoreText;
+
+    private int score;
+
+    //To add or subtract score, use
+    // private ScoreManager scoreManager;
+    //then
+    //scoreManager.Score++;
+    //scoreManager.Score--;
+
+
+    private void Update()
+    {
+        PlayerPrefs.SetInt("Player Score", score);
+        score = score + 1;
+    }
+
+    public void updateScoreView()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        PlayerPrefs.SetInt("player_score", score);
+    }
+
+    public void AddScore(int NewscoreValue)
+    {
+        score = score + 1;
+        updateScoreView();
+        PlayerPrefs.SetInt("player_score", score);
+    }
+
+
+}
+
